81d9ac89762316ac28b2b510788037a507b3eab1
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-03-17 19:27:18 +0700

    protect all admin resources

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,16 +5,14 @@ class Admin_Controller extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        /** 
-        $uid = $this->session->userdata('uid');
-        $fullname = $this->session->userdata('fullname');
-        $role = $this->session->userdata('role');
-        $id_kegiatan = $this->session->userdata('id_kegiatan');
-        if (!isset($uid) && !isset($fullname) && !isset($role) && !isset($id_kegiatan)) {
+        $id = $this->session->userdata('id');
+        $name = $this->session->userdata('nama');
+        $level = $this->session->userdata('level');
+        
+        if (!isset($id) && !isset($name) && !isset($level)) {
             $this->session->set_flashdata('warn', 'Silahkan login terlebih dahulu');
             return redirect('auth');             
         }
-        **/
     }
 
 }
